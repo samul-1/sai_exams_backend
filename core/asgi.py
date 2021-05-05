2997ebe51245fbe550cd32f1dd166e6cafd50047
samul-1 <bsamuele00@gmail.com>
2021-05-05 11:33:53 +0200

    fixed asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -8,17 +8,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
 
-import django
-import jsplatform.routing
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+import jsplatform.routing
+from channels.auth import AuthMiddlewareStack
 from jsplatform.socket_auth_middleware import TokenAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-django.setup()
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
