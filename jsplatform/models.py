b38e2c2e9513e03ab177748dba0526866b17b259
samul-1 <bsamuele00@gmail.com>
2021-07-24 12:36:52 +0200

    added signal to delete FileField files upon deletion of related model object

--- jsplatform/models.py
+++ jsplatform/models.py
@@ -987,3 +987,36 @@ class GivenAnswer(models.Model):
     #             "GivenAnswer's answer does not belong to the question"
     #         )
     #     return super().clean()
+
+
+"""
+To automatically remove files in model's FileField upon deletion
+"""
+
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+""" Whenever ANY model is deleted, if it has a file field on it, delete the associated file too"""
+
+
+@receiver(post_delete)
+def delete_files_when_row_deleted_from_db(sender, instance, **kwargs):
+    for field in sender._meta.concrete_fields:
+        if isinstance(field, models.FileField):
+            instance_file_field = getattr(instance, field.name)
+            print(f"deleting {instance_file_field}")
+            delete_file_if_unused(sender, instance, field, instance_file_field)
+
+
+""" Only delete the file if no other instances of that model are using it"""
+
+
+def delete_file_if_unused(model, instance, field, instance_file_field):
+    dynamic_field = {}
+    dynamic_field[field.name] = instance_file_field.name
+    other_refs_exist = (
+        model.objects.filter(**dynamic_field).exclude(pk=instance.pk).exists()
+    )
+    if not other_refs_exist:
+        instance_file_field.delete(False)
