8d316abb13c6ba5ba62176073b8cf3acf99c1e52
samul-1 <bsamuele00@gmail.com>
2021-09-25 15:11:31 +0200

    trying to fix cors problem

--- core/settings/base.py
+++ core/settings/base.py
@@ -91,11 +91,11 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    "jsplatform.custom_cors_middleware.CustomCorsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsPostCsrfMiddleware"
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
