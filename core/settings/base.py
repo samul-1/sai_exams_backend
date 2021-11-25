c2884232ca0077c9b6d95a434f4347bb5ae2a09f
samul-1 <bsamuele00@gmail.com>
2021-11-25 11:22:52 +0100

    added middleware to alert for 404

--- core/settings/base.py
+++ core/settings/base.py
@@ -101,6 +101,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.common.BrokenLinkEmailsMiddleware"
     # "django.middleware.common.CommonMiddleware",
 ]
 
